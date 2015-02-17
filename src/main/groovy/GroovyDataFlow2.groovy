b73d28ec3a8ab39a82b04f7cba6857477e422424
yaroslav.yermilov <yermilov17@gmail.com>
2015-02-18 01:54:01 +0200

    code formatting

--- src/main/groovy/GroovyDataFlow2.groovy
+++ src/main/groovy/GroovyDataFlow2.groovy
@@ -6,7 +6,8 @@ new GroovyShell(binding()).evaluate(
 '''
 def data = DataManger.loadData "data.csv", true, [ "int", "string", "date|yyyy-MM-dd", "boolean" ] as String[]
 data.findAll { it[2].after(DataManger.now()) && !it[3] }.collect { it[1] }.each { println it }
-''')
+'''
+)
 
 static Binding binding() {
     new Binding()
--- src/main/groovy/GroovyDataFlow3.groovy
+++ src/main/groovy/GroovyDataFlow3.groovy
@@ -6,7 +6,8 @@ new GroovyShell(binding()).evaluate(
 '''
 def data = "data.csv".loadData(true, [ "int", "string", "date|yyyy-MM-dd", "boolean" ])
 data.findAll { it[2].after(DataManger.now()) && !it[3] }.collect { it[1] }.each { println it }
-''')
+'''
+)
 
 static Binding binding() {
 
