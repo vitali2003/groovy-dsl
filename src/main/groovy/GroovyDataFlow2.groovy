568fc6f1dee7d1089ce6effb95ed8610eab4ab11
yaroslav.yermilov <yermilov17@gmail.com>
2015-02-18 01:49:28 +0200

    groovy dsl #2

---
+++ src/main/groovy/GroovyDataFlow2.groovy
@@ -0,0 +1,13 @@
+/**
+ * @author yaroslav.yermilov
+ */
+
+new GroovyShell(binding()).evaluate(
+'''
+def data = DataManger.loadData "data.csv", true, [ "int", "string", "date|yyyy-MM-dd", "boolean" ] as String[]
+data.findAll { it[2].after(DataManger.now()) && !it[3] }.collect { it[1] }.each { println it }
+''')
+
+static Binding binding() {
+    new Binding()
+}
