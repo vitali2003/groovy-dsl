e04052ee5694ab2107ac712be5bf81d272ad33f8
yaroslav.yermilov <yermilov17@gmail.com>
2015-02-18 01:53:31 +0200

    groovy dsl #3

---
+++ src/main/groovy/GroovyDataFlow3.groovy
@@ -0,0 +1,18 @@
+/**
+ * @author yaroslav.yermilov
+ */
+
+new GroovyShell(binding()).evaluate(
+'''
+def data = "data.csv".loadData(true, [ "int", "string", "date|yyyy-MM-dd", "boolean" ])
+data.findAll { it[2].after(DataManger.now()) && !it[3] }.collect { it[1] }.each { println it }
+''')
+
+static Binding binding() {
+
+    String.metaClass.loadData = { header, columnTypes ->
+        DataManger.loadData(delegate, header, columnTypes as String[])
+    }
+
+    new Binding()
+}
