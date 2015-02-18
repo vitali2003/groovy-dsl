f435c12e6be113444c4d1eea886f6f3c5cd9c2cb
yaroslav.yermilov <yermilov17@gmail.com>
2015-02-18 03:20:52 +0200

    code clean

--- src/main/groovy/GroovyDataFlow_06_Binding.groovy
+++ src/main/groovy/GroovyDataFlow_06_Binding.groovy
@@ -8,8 +8,6 @@ import ua.jug.dsl.groovy.DataManger.Row
 
 new GroovyShell(binding()).evaluate(
 '''
-import ua.jug.dsl.groovy.DataManger
-
 def data = 'data.csv'.loadData withHeader: true, ofTypes: [ 'int', 'string', 'date|yyyy-MM-dd', 'boolean' ]
 
 (data | { it['date'] > now() && !it['weekday'] }).collect { it['name'] }.each { println it }
