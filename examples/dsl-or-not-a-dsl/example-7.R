fcbd548753355948b56d6b0ffa550bbc2ca3d855
yaroslav.yermilov <yermilov17@gmail.com>
2015-04-28 00:45:09 +0300

    improve formatting

--- examples/dsl-or-not-a-dsl/example-7.R
+++ examples/dsl-or-not-a-dsl/example-7.R
@@ -1,3 +1,9 @@
-data <- read.table("data/household_power_consumption.txt", header = TRUE, sep = ";", colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'), na.strings = '?')
+data <- read.table("data/household_power_consumption.txt", 
+	header = TRUE, sep = ";", na.strings = '?',
+	colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+
 data <- data[data[, 'Date'] == '1/2/2007' | data[, 'Date'] == '2/2/2007', ]
-hist(data$'Global_active_power', col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency"))
+
+hist(data$'Global_active_power', 
+	col = "red", main = "Global Active Power", 
+	xlab = "Global Active Power (kilowatts)", ylab = "Frequency"))
