5a1813b4760625000fcb90eaa89013b125ed7202
yaroslav.yermilov <yermilov17@gmail.com>
2015-05-01 22:50:38 +0300

    final example

---
+++ src/main/groovy/09_Method_Chaining.groovy
@@ -0,0 +1,30 @@
+import com.jeeconf.groovydsl.Monitoring
+
+Integer.metaClass.propertyMissing = {String name ->
+    if (name == 'seconds') {
+        delegate * 1000
+    }
+}
+
+def binding = new Binding()
+
+binding.please = { Closure action ->
+    [ sms: { Map params ->
+        String phoneNumber = params.to
+        [ every: { long period ->
+            action.call(phoneNumber, period)
+        } ]
+    } ]
+}
+
+binding.send = { String phoneNumber, long period ->
+    Monitoring.sendStatus(phoneNumber, period)
+}
+
+binding.me = '380934902436'
+
+new GroovyShell(binding).evaluate(
+'''
+please send sms to:me every 30.seconds
+'''
+)
