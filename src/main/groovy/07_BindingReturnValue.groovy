ee823d4345c53debf757e9ae39f9571e4f9829af
yaroslav.yermilov <yermilov17@gmail.com>
2015-05-06 18:37:59 +0300

    binding return value example

---
+++ src/main/groovy/07_BindingReturnValue.groovy
@@ -0,0 +1,27 @@
+import com.jeeconf.groovydsl.Monitoring
+
+String.metaClass.leftShift = { Map params ->
+    long period = params?.every
+    int times = params.notMoreThan ?: 3
+    Monitoring.sendStatusPeriodically(delegate, period, times)
+}
+
+def binding = new Binding()
+
+binding.me = '380934902436'
+
+binding.schedule = { long period, int times ->
+    [ every : period, notMoreThan: times ]
+}
+
+new GroovyShell(binding).evaluate(
+'''
+import java.util.concurrent.TimeUnit
+
+period = TimeUnit.SECONDS.toMillis(10)
+me << schedule(period, 1)
+'''
+)
+
+println binding.period
+
